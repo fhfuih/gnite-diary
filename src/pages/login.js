7a524f9eec98b912952be2bcd04a66845ecca5cf
Zeyu Huang <fhfuih@outlook.com>
2020-11-15 22:43:52 +0800

    feat: add register button in login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -27,6 +27,9 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '2rem',
         width: '25ch'
     },
+    register: {
+        width: '25ch'
+    },
     large: {
         width: theme.spacing(10),
         height: theme.spacing(10),
@@ -51,11 +54,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function BasicTextFields() {
     const classes = useStyles();
-    const history = useHistory();
-
-    const handleSubmit = () => {
-        history.push(ROUTES.profile);
-    }
 
     return (
         <div className={classes.background}>
@@ -64,13 +62,14 @@ export default function BasicTextFields() {
                 <Avatar className={classes.large} alt="head picture" src="../logo512.png"/>
                 <TextField id="username-basic" label="User Name" />
                 <TextField id="password-basic" label="Password" type='password' />
-                <Button className={classes.submit} variant="contained"
-                    color="primary"
-                    onClick={handleSubmit}
-                > Log in </Button>
-            </form>  
-            
-        </div>  
+                <Button className={classes.submit} variant="contained" color="primary" component={Link} to={ROUTES.profile}>
+                    Log in
+                </Button>
+                <Button className={classes.register} color="primary" component={Link} to={ROUTES.register}>
+                    Register
+                </Button>
+            </form>
+        </div>
     );
 }
 
