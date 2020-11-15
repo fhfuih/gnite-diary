d68b9ba91d9c632e3dcd6abcb6fb5a3301525a7b
Dingdong-LIU <dliuak@connect.ust.hk>
2020-11-15 22:13:29 +0800

    feat: adjust font size and positions

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import ROUTES from '../constants/routes';
+import { Avatar } from '@material-ui/core';
 // import Box from '@material-ui/core/Box';
 
 // const alert_string = '雅蠛蝶, hentai, 死库水'
@@ -11,19 +12,25 @@ import ROUTES from '../constants/routes';
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
-        marginTop: '38.2%',
-        textAlign: 'center',
-        // color: '#939B97',
-        // height: '100%',
-        // display: 'flex',
-        // flexDirection: 'column',
-        // justifyContent: 'center',
-        // alignItems: 'center',
+        marginTop: '32.2%',
+        // textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         '& > *': {
             margin: theme.spacing(1),
             width: '25ch',
             
         },
+    },
+    submit: {
+        marginTop: '2rem',
+        width: '25ch'
+    },
+    large: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+        marginBottom: '1rem'
     },
 }));
 
@@ -36,14 +43,17 @@ export default function BasicTextFields() {
     }
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <TextField id="username-basic" label="User Name" />
-            <TextField id="password-basic" label="Password" type='password'/>
-            <Button className="Submit" variant="contained"
-                color="primary"
-                onClick={handleSubmit}
-            > Log in </Button>
-        </form>        
+        <div>
+            <form className={classes.root} noValidate autoComplete="off">
+                <Avatar className={classes.large} alt="head picture" src="../logo512.png"/>
+                <TextField id="username-basic" label="User Name" />
+                <TextField id="password-basic" label="Password" type='password' />
+                <Button className={classes.submit} variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                > Log in </Button>
+            </form>      
+        </div>  
     );
 }
 
