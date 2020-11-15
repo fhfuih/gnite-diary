4e23dbd41fb2f2815a8286fd2578d80f6d763d4c
Zeyu Huang <fhfuih@outlook.com>
2020-11-15 22:14:05 +0800

    feat: add theme color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,33 +15,46 @@ import Login from './pages/login';
 import Register from './pages/register';
 import Navi from './components/bottom-navigation';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4B6A50'
+    },
+    secondary: {
+      main: '#EEE1C1',
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path={ROUTES.login}>
-          <Login />
-        </Route>
-        <Route path={ROUTES.register}>
-          <Register />
-        </Route>
-        <Route path={ROUTES.profile}>
-          <Profile />
-          <Navi/>
-        </Route>
-        <Route path={ROUTES.diary}>
-          <Diary />
-          <Navi/>
-        </Route>
-        <Route path={ROUTES.test}>
-          <Test />
-          <Navi/>
-        </Route>
-        <Route path={ROUTES.root}>
-          <Redirect to={ROUTES.login} />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path={ROUTES.login}>
+            <Login />
+          </Route>
+          <Route path={ROUTES.register}>
+            <Register />
+          </Route>
+          <Route path={ROUTES.profile}>
+            <Profile />
+            <Navi/>
+          </Route>
+          <Route path={ROUTES.diary}>
+            <Diary />
+            <Navi/>
+          </Route>
+          <Route path={ROUTES.test}>
+            <Test />
+            <Navi/>
+          </Route>
+          <Route path={ROUTES.root}>
+            <Redirect to={ROUTES.login} />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
